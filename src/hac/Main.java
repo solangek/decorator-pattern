f299dedb529175adfe39bf9d2e4b4976df6d9f4e
Sol <sodanabe@gmail.com>
2022-03-18 10:45:14 +0200

    added run configuration

--- src/hac/Main.java
+++ src/hac/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Decorating the component 1: ");
+        System.out.println("The original component:");
+        new ConcreteComponent().operation();
+
+        System.out.println("Decorating the component: ");
 
         // compose your decorators as you want
         Decorator decorator = new ConcreteDecorator2 (new ConcreteDecorator1(new ConcreteComponent()));
