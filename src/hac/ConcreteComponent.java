09ca684a98eb8bd67f6ad0001ebf2eb83ce82b86
Sol <sodanabe@gmail.com>
2023-03-08 00:34:16 +0200

    2023

--- src/hac/ConcreteComponent.java
+++ src/hac/ConcreteComponent.java
@@ -4,6 +4,8 @@ public class ConcreteComponent implements Component {
     String data;
 
     public ConcreteComponent(String data) {
+        // in a real world application, this would be for example a file name
+        // and data would be the contents of the file
         this.data = data;
     }
     @Override
